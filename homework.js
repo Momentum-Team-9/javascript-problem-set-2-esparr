02dd3b814384c1a2fa1204f68f1c8c9152e75e19
Em <amberem@gmail.com>
2021-07-13 22:21:09 -0400

    I tried my best.

--- homework.js
+++ homework.js
@@ -2,30 +2,62 @@
 // member of the array, and returns a new array with that member removed.
 // For example, `remove(['Cadence', 'Ordel', 'Marion'], 'Marion')` results
 // in `['Cadence', 'Ordel']`.
+//
+// If the potential member is not in the array, return the array unchanged.
+// If the potential member is in the array, remove all instances of it from the array.
 function remove(names, removedName) {
-    const idx = names.indexOf(removedName);
     for (let i = 0; i < names.length; i++) {
+        let idx = names.indexOf(removedName);
         if (i === idx) {
            names.splice(idx, 1);
         }
     }
-    return names
+    return names;
 }
-//
-// If the potential member is not in the array, return the array unchanged.
-// If the potential member is in the array, remove all instances of it from the array.
-
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
+
+function remove(names, removedName) {
+    let newNames = [...names]  // copies names array
+    for (let i = 0; i < newNames.length; i++) {
+        let idx = newNames.indexOf(removedName);
+        if (i === idx) {
+           newNames.splice(idx, 1);
+        }
+    }
+    return newNames
+}
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
 
+function sum(nums) {
+    return nums.reduce(function (total,num) {
+        return total + num
+    }, 0)
+}
+
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
 
+function average(nums) {
+    if (nums.length > 0) {
+        return sum(nums) / nums.length
+    } else {
+        return undefined
+    }
+}
+
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
+function minimum(nums) {
+    if (nums.length !== 0) {
+        return Math.min(...nums)
+    } else {
+         undefined
+     } 
+}
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -51,8 +83,31 @@ function remove(names, removedName) {
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
 
+function selectionSort(numbers) {
+    let newNumbers = [...numbers]  // copies names array
+    for (let i = 0; i < newNumbers.length; i++){
+        if (numbers.length > -1) {
+            let minNumber = minimum(numbers)
+            newNumbers.push(minNumber)
+            let numbers = remove(numbers, minNumber)
+            return newNumbers 
+        } else {
+            return null
+        }
+    }
+}
+    
+
 // 7. Create a function called `textList` that takes an array and joins its elements
 // into a string separated by commas.
 //
 // For example, `textList(['Cadence', 'Ordel', 'Marion'])` results in the string
 // `"Cadence,Ordel,Marion"`.
+
+function textList(names) {
+    if (names.length > 0) {
+        return document.write(names)
+    } else {
+        undefined
+    }
+}
